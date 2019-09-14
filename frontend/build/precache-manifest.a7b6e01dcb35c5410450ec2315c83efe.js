ad3f31a8650977e77768cb78d9f83a81979bd4cc
degun <pretty.lonely.place@hotmail.com>
2019-09-14 17:50:57 +0200

    second frontend  deploy

---
+++ frontend/build/precache-manifest.a7b6e01dcb35c5410450ec2315c83efe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce088353a6598fbdd6112d541686c8a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11a103da03bd6bc09745",
+    "url": "/static/css/main.415eba6b.chunk.css"
+  },
+  {
+    "revision": "b7a1e8ff501cdb217409",
+    "url": "/static/js/2.631b0306.chunk.js"
+  },
+  {
+    "revision": "11a103da03bd6bc09745",
+    "url": "/static/js/main.76d17fc2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "71e6b608b9dc9429613828caa3995eca",
+    "url": "/static/media/Chimera_01-01.71e6b608.svg"
+  }
+]);
