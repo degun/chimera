300aa78139c2709c38af82e1183162eb2ea9bef7
degun <pretty.lonely.place@hotmail.com>
2019-09-16 12:46:42 +0200

    added again 9000 port to frontend

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5018050c4e8376d91a49b9a7ff4e8ffb.js"
+  "/precache-manifest.000c57ebd0a3a056e9e2877569bc51e4.js"
 );
 
 self.addEventListener('message', (event) => {
