4c03aac66460e50c0c72ac8abbd650ebe5195d42
degun <pretty.lonely.place@hotmail.com>
2019-09-16 16:46:01 +0200

    remove port 9000 from build

--- frontend/build/precache-manifest.5018050c4e8376d91a49b9a7ff4e8ffb.js
+++ frontend/build/precache-manifest.5018050c4e8376d91a49b9a7ff4e8ffb.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2c0e91d4ea71eae795af6070aa829165",
+    "revision": "5b8e02a9a27cc00b5dc2c27dd7126d1a",
     "url": "/index.html"
   },
   {
-    "revision": "0cc01a6b72f561fe5c2a",
+    "revision": "4e1d90283812693023d8",
     "url": "/static/css/main.415eba6b.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.631b0306.chunk.js"
   },
   {
-    "revision": "0cc01a6b72f561fe5c2a",
-    "url": "/static/js/main.7aa0fa6f.chunk.js"
+    "revision": "4e1d90283812693023d8",
+    "url": "/static/js/main.9d74e72e.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
