174a84f0a0032f2a6b8c72684f05f5b2f66dd0b5
degun <pretty.lonely.place@hotmail.com>
2019-09-16 16:43:21 +0200

    try asgi with django setup

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,6 +1,9 @@
 import os
+import django
+from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from api.models import User
