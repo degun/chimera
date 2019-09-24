a658d880e712acdad98463cbaee7bce2035ad914
degun <pretty.lonely.place@hotmail.com>
2019-09-24 10:44:28 +0200

    email host settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -137,9 +137,9 @@ JWT_AUTH = {
 #mail
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'localhost'
+EMAIL_HOST = 'mail.chimera-finance.com'
 EMAIL_PORT = 25
-EMAIL_HOST_USER = ''
+EMAIL_HOST_USER = 'degun'
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = False
 DEFAULT_FROM_EMAIL = 'Admin <admin@chimera-finance.com>'
