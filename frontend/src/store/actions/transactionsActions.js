0983121238c55ed5f1698bbd34477ddf42061ce1
degun <pretty.lonely.place@hotmail.com>
2021-04-22 10:30:36 +0200

    fix: update user balance on transaction delete

--- frontend/src/store/actions/transactionsActions.js
+++ frontend/src/store/actions/transactionsActions.js
@@ -104,7 +104,7 @@ export const addTransaction = (transaction_type, client_name, amount, amount_pai
         dispatch({ type: actionTypes.TRANSACTIONS_LOADING, loading: true })
         const state = getState();
         const {users} = state.users;
-        const user0 = users.filter(u=> u.id === user)[0];
+        const user0 = users.filter(u=> u.id === user.toString())[0];
         const {username} = user0;
         const {token} = state.auth;
         const bearer = 'Bearer ' + token;
@@ -128,7 +128,6 @@ export const addTransaction = (transaction_type, client_name, amount, amount_pai
                 default: message = `Made transaction of type ${transaction_type}. Partner ${username}, main amount ${adminAmount}, partner amount ${partnerAmount} (because rate is ${rate}).`;break;
             }
             dispatch(addLog(user, logTypes.TRANSACTION_ADD, message))
-            dispatch(getAllTransactions());
             dispatch({ type: actionTypes.TRANSACTIONS_LOADING, loading: false })
         }).catch(e => {
             dispatch({type: actionTypes.TRANSACTIONS_ADD_FAIL, e})
@@ -167,7 +166,6 @@ export const editTransaction = (transaction_type, client_name, amount, amount_pa
                 default: message = `Made transaction of type ${transaction_type}. Partner ${username}, main amount ${adminAmount}, partner amount ${partnerAmount} (because rate is ${rate}).`;break;
             }
             dispatch(addLog(user, logTypes.TRANSACTION_ADD, message))
-            dispatch(getAllTransactions());
             dispatch({ type: actionTypes.TRANSACTIONS_LOADING, loading: false })
         }).catch(e => {
             dispatch({type: actionTypes.TRANSACTIONS_ADD_FAIL, e})
@@ -194,10 +192,10 @@ export const removeTransaction = id => {
             const partnerAmount = numeral(parseFloat(amount_paid)).format('0,0.00 $');
             dispatch({type: actionTypes.TRANSACTIONS_REMOVE_SUCCESS, id});
             dispatch({type: actionTypes.TRANSACTIONS_END_EDIT});
+            dispatch(updateUserLocally(user));
             dispatch(updateAdminLocally());
             const message = `Removed transaction ${id} of type ${transaction_type}. Client: ${client_name}. Main amount: ${adminAmount}. Partner amount: ${partnerAmount}. Partner: ${username}. Rate ${rate}`;
             dispatch(addLog(user, logTypes.TRANSACTION_REMOVE, message))
-            dispatch(getAllTransactions());
             dispatch({ type: actionTypes.TRANSACTIONS_LOADING, loading: false })
         })
         .catch(e => {
