987c2d2483eaaefd44fd3099fc107919e50ab000
degun <metamorphine.virus@gmail.com>
2021-04-23 19:46:59 +0200

    log error

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -190,6 +190,14 @@ LOGGING = {
             'backupCount': 5,
             'formatter':'standard',
         },  
+        'errors': {
+            'level':'ERROR',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': '/var/log/django/error.log',
+            'maxBytes': 1024*1024*5, # 5 MB
+            'backupCount': 5,
+            'formatter':'standard',
+        },  
         'request_handler': {
                 'level':'DEBUG',
                 'class':'logging.handlers.RotatingFileHandler',
@@ -204,6 +212,11 @@ LOGGING = {
         '': {
             'handlers': ['default'],
             'level': 'DEBUG',
+            'propagate': True
+        },
+        'django': {
+            'handlers': ['error'],
+            'level': 'ERROR',
             'propagate': True
         },
         'django.request': { # Stop SQL debug from logging to main logger
